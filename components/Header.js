181cd1604e43d2d9ff0e0bc5e8c6be76c2c7cab9
ziminliu <liuziminliuzimin@gmail.com>
2020-05-05 23:09:55 +0800

    Header 组件的样式修改

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ import {
 const Header = () => (
   <div className="header">
     <Row type="flex" justify="center">
-      <Col xs={24} sm={24} md={10} lg={10} xl={10}>
+      <Col xs={24} sm={24} md={10} lg={15} xl={12}>
         <span className="header-logo">技术胖</span>
         <span className="header-txt">专注前端开发</span>
       </Col>
