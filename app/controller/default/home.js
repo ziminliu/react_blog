ece6e42d851c510549ffbc54ebbcddf197e8c85d
ziminliu <liuziminliuzimin@gmail.com>
2020-05-10 15:26:20 +0800

    提供跨域访问单篇文章

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -18,7 +18,7 @@ class HomeController extends Controller {
       'FROM article LEFT JOIN type ON article.type_id=type.id';
     const results = await this.app.mysql.query(sql);
     this.ctx.body = { data: results };
-    console.log(results);
+    // console.log(results);
   }
   async getArticleById() {
     const id = this.ctx.params.id;
@@ -29,12 +29,11 @@ class HomeController extends Controller {
       'article.article_content as article_content,' +
       "FROM_UNIXTIME(article.addTime,'%Y-%m-%d %H:%i:%s') as addTime," +
       'article.view_count as view_count,' +
-      'type.typeName as typeName ' +
+      'type.typeName as typeName ,' +
       'type.id as typeId ' +
       'FROM article LEFT JOIN type ON article.type_id=type.id ' +
       'WHERE article.id=' +
       id;
-    console.log(sql);
     const result = await this.app.mysql.query(sql);
     this.ctx.body = { data: result };
   }
