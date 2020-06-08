2b352fc8ee81a92ebac49c74feb00d144e83ac30
ziminliu <liuziminliuzimin@gmail.com>
2020-06-08 11:00:52 +0800

    整合

---
+++ server/components/Advert.js
@@ -0,0 +1,11 @@
+import "../public/style/components/advert.css";
+
+const Advert = () => {
+  return (
+    <div className="ad-div comm-box">
+      
+    </div>
+  );
+};
+
+export default Advert;
---
+++ server/components/Author.js
@@ -0,0 +1,27 @@
+import { Avatar, Divider } from "antd";
+import { GithubOutlined, QqOutlined, WechatOutlined } from "@ant-design/icons";
+import "../public/style/components/author.css";
+const Author = () => {
+  return (
+    <div className="author-div comm-box">
+      <div>
+        <Avatar size={100} src="/avator.jpg" />
+      </div>
+      <div className="author-introduction">
+        专注于WEB和移动前端开发
+        <Divider>社交账号</Divider>
+        <Avatar size={28} className="account">
+          <GithubOutlined />
+        </Avatar>
+        <Avatar size={28} className="account">
+          <QqOutlined />
+        </Avatar>
+        <Avatar size={28} className="account">
+          <WechatOutlined />
+        </Avatar>
+      </div>
+    </div>
+  );
+};
+
+export default Author;
